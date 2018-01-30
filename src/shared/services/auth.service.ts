9f7584ae4df453e571f340f6454667d0867430e9
Fortozo <yannick.sprenger@gmail.com>
2018-01-30 16:38:02 -0600

    Last change to mockusers and auth.service

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -9,30 +9,10 @@ export class AuthService {
 
   constructor() { }
 
-<<<<<<< HEAD
   checkEmail(email: string): Observable<boolean>  {
   const Avaliable = emails.filter( user => user.email === email).length === 0;
   return Observable.of(Avaliable).delay(1600);
   }
 
-=======
-  getCurrentUser(): string {
-    return localStorage.getItem('CurrentUser');
-  }
-
-  setCurrentUser(user: string): void {
-      localStorage.setItem('CurrentUser', user);
-  }
-
-  logoutUser(): void {
-    localStorage.setItem('CurrentUser', '');
-  }
-
-  verifyPassword(email: string, password: string): boolean {
-    const userindex = users.findIndex(x => x.email === email);
-    return password === users[userindex].password  ? true : false;
-  }
-
->>>>>>> 46ec53936a51fcebebf8b8eae9b6960e70f5d50c
 }
 
