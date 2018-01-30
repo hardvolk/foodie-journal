46ec53936a51fcebebf8b8eae9b6960e70f5d50c
Carlos Baez <c.baez.de.hoyos@accenture.com>
2018-01-30 16:09:48 -0600

    tslint fixes

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -2,27 +2,26 @@ import { Injectable } from '@angular/core';
 
 import { users } from './mockusers';
 
-
 @Injectable()
 export class AuthService {
 
   constructor() { }
 
-  getCurrentUser():string {
+  getCurrentUser(): string {
     return localStorage.getItem('CurrentUser');
   }
 
-  setCurrentUser(user:string): void {
+  setCurrentUser(user: string): void {
       localStorage.setItem('CurrentUser', user);
-    }
-
-  verifyPassword(email: string, password:string): boolean {
-      let userindex = users.findIndex(x => x.email === email);
-      return password === users[userindex].password  ? true : false;
-    }
+  }
 
   logoutUser(): void {
-      localStorage.setItem('CurrentUser', '');
-    }
+    localStorage.setItem('CurrentUser', '');
+  }
+
+  verifyPassword(email: string, password: string): boolean {
+    const userindex = users.findIndex(x => x.email === email);
+    return password === users[userindex].password  ? true : false;
+  }
 
 }
