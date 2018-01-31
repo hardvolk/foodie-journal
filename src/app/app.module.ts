ffec64dd5e5a58922c85a4b3d37dfcdfc02ea06f
PapitomasterAccenture <35741078+PapitomasterAccenture@users.noreply.github.com>
2018-01-31 16:47:55 -0600

    Added tracks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AuthService } from '../shared/services/auth.service'
+import { AuthService } from '../shared/services/auth.service';
 import { AppComponent } from './app.component';
 import { HomeMainMenuComponent } from './home-main-menu/home-main-menu.component';
-
+import { TracksComponent } from './tracks/tracks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeMainMenuComponent
+    HomeMainMenuComponent,
+    TracksComponent
   ],
   imports: [
     BrowserModule
