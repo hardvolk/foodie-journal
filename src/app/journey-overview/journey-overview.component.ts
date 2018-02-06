ac12ec2081e31d149e4251eed1a919e992962bab
Ibarra <cesar.ibarra@accenture.com>
2018-02-06 17:14:17 -0600

    Modify Journey Overview

--- src/app/journey-overview/journey-overview.component.ts
+++ src/app/journey-overview/journey-overview.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JourneyOverviewComponent implements OnInit {
 
+  // Track
+  track: any = {
+    unitsCcompleted: 1,
+    unitsTotal: 10,
+    iconUrl: '/assets/icons/tracks/burger.png',
+    title: 'American',
+    description: 'One characteristic of American cooking is the fusion of multiple ethnic ' +
+      'or regional approaches into completely new cooking styles.',
+    duration: '3 weeks',
+  };
+
   constructor() { }
 
   ngOnInit() {
