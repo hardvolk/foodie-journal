2085a556394a922229d0009421e39e6ebe570d83
Fortozo <yannick.sprenger@gmail.com>
2018-02-08 15:11:59 -0600

    agregando diseño a template de journey-overview

--- src/app/journey-overview/journey-overview.component.ts
+++ src/app/journey-overview/journey-overview.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { journeys as JourneyDS } from '../../shared/interfaces/journeys';
+import { ApiService } from '../../shared/services/api.service';
+import { Restaurant } from '../../shared/interfaces/restaurant';
 
 @Component({
   selector: 'app-journey-overview',
@@ -9,9 +11,10 @@ import { journeys as JourneyDS } from '../../shared/interfaces/journeys';
 })
 export class JourneyOverviewComponent implements OnInit {
 
-  constructor(private _activatedRoute: ActivatedRoute) { }
+  constructor(private _activatedRoute: ActivatedRoute, private _apiService: ApiService) { }
   journeyParams: any;
   journey: any;
+  restaurante: Restaurant[];
 
   ngOnInit() {
     this._activatedRoute.params.subscribe(params => {
@@ -19,6 +22,6 @@ export class JourneyOverviewComponent implements OnInit {
       this.journey = JourneyDS.find( j => j.name === params.trackId);
       console.log('Current Journey: ', this.journey);
     });
+    // this._apiService.getRestaurantInfo()
   }
-
 }
