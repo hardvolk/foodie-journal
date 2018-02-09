2140b0941352ee566b48a8877cda141c679e3678
Carlos Baez <c.baez.de.hoyos@accenture.com>
2018-02-09 10:13:17 -0600

    merge iteration one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,11 @@ import { ApiService } from '../shared/services/api.service';
 import { SafePipe } from '../shared/pipes/safe.pipe';
 import { UserService } from '../shared/services/user.service';
 import { ProgressbarModule } from 'ngx-bootstrap';
-<<<<<<< HEAD
 import { TrackComponent } from './tracks/track/track.component';
 import { ProgressBoxComponent } from './tracks/progress-box/progress-box.component';
 import { DishDetailComponent } from './dish/dish-detail/dish-detail.component';
-import { PopoverModule } from 'ngx-bootstrap/popover';
-=======
 import { HomeMainMenuComponent } from './home-main-menu/home-main-menu.component';
 import { AppRoutingModule, routingComponents } from './app.routing.module';
->>>>>>> 9ebe9cad59cd97c4761231b85c53cfb604784c3d
 
 
 @NgModule({
--- src/shared/interfaces/user.ts
+++ src/shared/interfaces/user.ts
@@ -2,7 +2,6 @@ export interface User {
     name: string;
     email: string;
     password: string;
-<<<<<<< HEAD
     journeys?: {
         0?: boolean[];
         1?: boolean[];
@@ -10,12 +9,4 @@ export interface User {
         3?: boolean[];
         4?: boolean[];
     };
-=======
-    currentJourney?: string;
-    journey1?: boolean[];
-    journey2?: boolean[];
-    journey3?: boolean[];
-    journey4?: boolean[];
-    journey5?: boolean[];
->>>>>>> 9ebe9cad59cd97c4761231b85c53cfb604784c3d
 }
