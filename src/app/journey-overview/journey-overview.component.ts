84b921908c4fdbff5bbbadb41d2d5339a3d75bb7
Fortozo <yannick.sprenger@gmail.com>
2018-02-10 02:43:58 -0600

    commit alv para hacer pull request a iteration-one ALV

--- src/app/journey-overview/journey-overview.component.ts
+++ src/app/journey-overview/journey-overview.component.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { journeys as JourneyDS, journeys } from '../../shared/interfaces/journeys';
 import { ApiService } from '../../shared/services/api.service';
 import { Restaurant } from '../../shared/interfaces/restaurant';
+import { users } from '../../shared/interfaces/mockusers';
+import { User } from '../../shared/interfaces/user';
+import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-journey-overview',
@@ -11,19 +14,26 @@ import { Restaurant } from '../../shared/interfaces/restaurant';
 })
 export class JourneyOverviewComponent implements OnInit {
 
-  constructor(private _activatedRoute: ActivatedRoute, private _apiService: ApiService) { }
+  constructor(private _activatedRoute: ActivatedRoute, private _apiService: ApiService, private _userService: UserService) { }
   journeyParams: any;
   journey: any;
   res: Restaurant[];
-  loading = true;
+  // rest: Restaurant;
   journeyInput: String;
   dish: number;
+  user: User = this._userService.LoggedUser.value;
 
-  getDishDetail(journey: String, dish: number) {
-    this._apiService.getRestaurantInfo(journeys.find(x => x.name === journey).dishrest[dish]).subscribe(x => {
-      this.res[dish] = x as Restaurant;
-     });
-  }
+  // getDishDetail(journey: String, dish: number) {
+  //   this._apiService.getRestaurantInfo(journeys.find(x => x.name === journey).dishrest[dish]).subscribe(x => {
+  //     this.res[dish] = x as Restaurant;
+  //    });
+  // }
+
+  // getDishDetail2(journey: number, dish: number) {
+  //   this._apiService.getRestaurantInfo(journeys[journey].dishrest[dish]).subscribe(x => {
+  //     this.rest = x as Restaurant;
+  //    });
+  // }
 
   ngOnInit() {
     this._activatedRoute.params.subscribe(params => {
@@ -33,4 +43,3 @@ export class JourneyOverviewComponent implements OnInit {
     });
   }
 }
-
