9a06ed2a512cd1ee97b87ec147b75ba5c08f568e
yannicksprenger <yannick.sprenger@gmail.com>
2018-02-09 20:44:30 -0600

    Revert "Capitan journey list"

--- src/shared/interfaces/user.ts
+++ src/shared/interfaces/user.ts
@@ -2,12 +2,9 @@ export interface User {
     name: string;
     email: string;
     password: string;
-    currentJourney?: string;
-    journeys?: {
-        0?: boolean[];
-        1?: boolean[];
-        2?: boolean[];
-        3?: boolean[];
-        4?: boolean[];
-    };
+    journey1?: boolean[];
+    journey2?: boolean[];
+    journey3?: boolean[];
+    journey4?: boolean[];
+    journey5?: boolean[];
 }
--- src/shared/services/user.service.ts
+++ src/shared/services/user.service.ts
@@ -1,51 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { last } from 'rxjs/operators/last';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
 import { User } from '../interfaces/user';
-import { map } from 'rxjs/operator/map';
 
 @Injectable()
 export class UserService {
-  LoggedUser = new BehaviorSubject<User>({name: '', email: '', password: ''});
-  user: User = {name: '', email: '', password: ''};
+  LoggedUser = new ReplaySubject<User>();
+  user: User;
 
   constructor() {}
 
-  isAuthenticated(): boolean {
-    return localStorage.getItem('CurrentUser') !== null;
-  }
-
-  checkInitialUser(): BehaviorSubject<User> {
+  checkInitialUser() {
     if (!!localStorage.getItem('CurrentUser')) {
       this.login(JSON.parse(localStorage.getItem(localStorage.getItem('CurrentUser'))));
       } else { this.LoggedUser.next({name: '', email: '', password: ''}); }
-    return this.LoggedUser;
   }
 
   login (user: User) {
     if (user.name.length > 0) {
       localStorage.setItem('CurrentUser', user.name);
-      localStorage.setItem(user.name, JSON.stringify(user));
-      this.user = user;
+      if (!!localStorage.getItem(user.name)) {
+        this.user = JSON.parse(localStorage.getItem(user.name));
+      } else {
+        localStorage.setItem(user.name, JSON.stringify(user));
+      }
+      console.log('User ' + user.name + ' has logged in.');
     }
     this.LoggedUser.next(this.user);
   }
 
   logout (): void {
-    localStorage.removeItem(this.user.name); // We need to delete the user info from localStorage
     localStorage.removeItem('CurrentUser');
+    console.log('User has logged out.');
   }
 
   updateProgress (journeyid: number, dishid: number): void {
-      this.LoggedUser.value.journeys[journeyid][dishid] = !this.LoggedUser.value.journeys[journeyid][dishid];
-      localStorage.setItem(this.LoggedUser.value.name, JSON.stringify(this.LoggedUser.value));
-      this.LoggedUser.next(this.LoggedUser.value);
-      console.log('Changed ' + journeyid + ', ' + dishid + ' to value: ' + this.user.journeys[journeyid][dishid]); /* }); */
-  }
-
-  checkProgress (journeyid: number): number {
-    return this.LoggedUser.value.journeys[journeyid].filter( x => x === true).length;
+    this.LoggedUser.subscribe(user => (this.user = user));
+    this.user['journey' + journeyid][dishid - 1] = !this.user['journey' + journeyid][dishid - 1];
+    this.LoggedUser.next(this.user);
+    localStorage.setItem(this.user.name, JSON.stringify(this.user));
+    console.log('Changed journey ' + journeyid + ' , dish ' + dishid + ' to value: ' + this.user['journey' + journeyid][dishid - 1]);
   }
 
 }
