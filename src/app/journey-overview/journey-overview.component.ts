91a994e98b4faa579b172a145617e5cc57ef5c2d
Fortozo <yannick.sprenger@gmail.com>
2018-02-12 15:13:13 -0600

    adding function to accordion

--- src/app/journey-overview/journey-overview.component.ts
+++ src/app/journey-overview/journey-overview.component.ts
@@ -4,6 +4,7 @@ import { journeys as JourneyDS, journeys } from '../../shared/interfaces/journey
 import { ApiService } from '../../shared/services/api.service';
 import { Restaurant } from '../../shared/interfaces/restaurant';
 import { UserService } from '../../shared/services/user.service';
+import { Review } from '../../shared/interfaces/review';
 
 @Component({
   selector: 'app-journey-overview',
@@ -20,17 +21,27 @@ export class JourneyOverviewComponent implements OnInit {
   viajes = journeys;
   dish: number;
   loading: boolean[] = [false, false, false, false, false, false, false, false, false, false];
+  // review: Review;
+  // loadingReview: boolean;
 
   getDishDetail(journey: String, dish: number) {
     this._apiService.getRestaurantInfo(journeys.find(x => x.name === journey).dishrest[dish]).subscribe(x => {
       this.res[dish] = x as Restaurant;
       this.loading[dish] = true;
+      console.log(this.res);
      });
     }
 
   getYelpStars(dish: number): string {
     return '/assets/images/yelp-stars/' + String(Math.round(this.res[dish].rating * 2) / 2) + '.png';
     }
+
+  // getReview(journey: number, dish: number) {
+  //     const obs2 = this._apiService.getRestaurantReview(journeys[journey].dishrest[dish]).subscribe( x => {
+  //       this.review = x as Review;
+  //       if (this.review.reviews[0] !== undefined) { this.loadingeview = false; }
+  //     });
+  //   }
 
   ngOnInit() {
     this._activatedRoute.params.subscribe(params => {
